0a02674a3cdcc74cf4fe38098c4dfd4fd8a3798c
resmh <82471675+resmh@users.noreply.github.com>
2021-10-30 01:16:53 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -20,7 +20,7 @@ INPUT_EMPTY=${INPUT_EMPTY:-false}
 INPUT_DIRECTORY=${INPUT_DIRECTORY:-'.'}
 REPOSITORY=${INPUT_REPOSITORY:-$GITHUB_REPOSITORY}
 
-echo "Push to branch $INPUT_BRANCH of $REPOSITORY";
+echo 'Push to branch $INPUT_BRANCH of $REPOSITORY';
 [ -z "${INPUT_GITHUB_TOKEN}" ] && {
     echo 'Missing input "github_token: ${{ secrets.GITHUB_TOKEN }}".';
     exit 1;
@@ -40,8 +40,8 @@ fi
 
 cd "${INPUT_DIRECTORY}"
 
-remote_repo="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${REPOSITORY}.git"
 #remote_repo="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${REPOSITORY}.git"
+remote_repo="ssh://git@github.com/${REPOSITORY}.git"
 
 git config http.sslVerify false
 git config --local user.email "${INPUT_AUTHOR_EMAIL}"
